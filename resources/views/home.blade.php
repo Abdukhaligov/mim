921dc782bfef7e7fa0882b0954e3edfe6195146c
Abdukhaligov <hikmat.pou@gmail.com>
2020-09-24 07:29:01 +0400

    Add UserResource & change view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">{{ __('Dashboard') }}</div>
+          <div class="card-body">
+            @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+            @endif
+            @if(!$data["user"]->subscriber)
+              <h1>Please subscribe to this channels</h1>
+              @foreach($data["user"]->requiredSubscriptions as $sponsor)
+                <a target="_blank" href="{{$sponsor->link}}">{{$sponsor->name}}</a>
+              @endforeach
+            @else
+              <h1>Your account is activated</h1>
+            @endif
+            <br>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
